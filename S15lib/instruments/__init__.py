d7aac843e503ac3ad5b19b1ed1b1d6f9f716415f
Mathias Seidler <1469961+mathiasseidler@users.noreply.github.com>
2020-08-07 18:54:30 +0800

    added single photon detector

--- S15lib/instruments/__init__.py
+++ S15lib/instruments/__init__.py
@@ -2,3 +2,4 @@ from .powermeter import PowerMeter
 from .serial_connection import SerialConnection
 from .usb_counter_fpga import TimeStampTDC1
 from .stepper_motor_driver import StepperMotorDriver
+from .single_photon_detector import SinglePhotonDetector

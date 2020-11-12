39556508a51f3366b6fd87e2ba894b68fd955c05
Mathias Seidler <1469961+mathiasseidler@users.noreply.github.com>
2020-11-12 11:43:00 +0800

    Housekeeping

--- S15lib/apps/align_source.py
+++ S15lib/apps/align_source.py
@@ -1,18 +1,12 @@
+from datetime import datetime
+import os
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from S15lib.g2lib import g2lib
+from S15lib.instruments import TimeStampTDC1
 import sys
 print(sys.version)
-
-from S15lib.instruments import TimeStampTDC1
-from S15lib.g2lib import g2lib
-
-import numpy as np
-import matplotlib.pyplot as plt
-import time
-import os
-import pylab
-from datetime import datetime
-
-
-
 
 
 def show_source_properties(dev_path: str = None, logging: bool = True, ch_stop_delay: float = 5, t_acq: float = 1):
@@ -21,18 +15,13 @@ def show_source_properties(dev_path: str = None, logging: bool = True, ch_stop_d
         dev = TimeStampTDC1()
     else:
         dev = TimeStampTDC1(dev_path)
-    # dev.mode = 'singles'
-    # dev.time= 1
-    # print('Singles counts', dev.get_counts())
     file_time_str = datetime.now().isoformat()
     plt.figure()
     hl, = plt.plot([], [], '.-')
-
-    # time.sleep(1)
     error_counter = 0
     while True:
         try:
-            info, dt, pairs = dev.count_g2(t_acq, ch_stop_delay = ch_stop_delay)
+            info, dt, pairs = dev.count_g2(t_acq, ch_stop_delay=ch_stop_delay)
             acq_time = int(info['total_time']) * 1e-9
             os.system('clear')
             print('Acquisition time {:.3f} s'.format(acq_time))
@@ -51,10 +40,13 @@ def show_source_properties(dev_path: str = None, logging: bool = True, ch_stop_d
             efficiency_ch2 = pair_rate / rate_ch2
 
             print('Accidentals per bin (1/s): {:.0f}'.format(acc_rate_per_bin))
-            print('Pair rate corrected for accidentals (1/s): {:.0f}'.format(pair_rate))
+            print(
+                'Pair rate corrected for accidentals (1/s): {:.0f}'.format(pair_rate))
             print('Avg. efficiency: {:.2f}%'.format(efficiency * 100))
-            print('Singles channel1 (1/s): {:.0f}\tSingles channel2 (1/s): {:.0f}'.format(rate_ch1, rate_ch2))
-            print('pairs / acc: {:.2f}'.format((pair_rate / (2 * acc_rate_per_bin))))
+            print(
+                'Singles channel1 (1/s): {:.0f}\tSingles channel2 (1/s): {:.0f}'.format(rate_ch1, rate_ch2))
+            print(
+                'pairs / acc: {:.2f}'.format((pair_rate / (2 * acc_rate_per_bin))))
             print('ch1 heralding eff.: {:.2f}%\tch2 heralding eff.: {:.2f}%'.format(
                 efficiency_ch1 * 100, efficiency_ch2 * 100))
 
@@ -66,22 +58,20 @@ def show_source_properties(dev_path: str = None, logging: bool = True, ch_stop_d
 
             hl.set_xdata(dt)
             hl.set_ydata(pairs)
-            # plt.plot(dt, pairs, 'o-')
             plt.xlim(-10, 100)
             plt.ylim(0, np.max(pairs) + 10)
             plt.draw()
             plt.pause(0.01)
-            # time.sleep(59)
         except Exception as a:
             print(type(a))
             error_counter += 1
             if error_counter == 10:
-            	print('too many errors in a row')
-            	break
+                print('too many errors in a row')
+                break
         finally:
-        	error_counter = 0
+            error_counter = 0
 
 
 if __name__ == '__main__':
-    show_source_properties(dev_path = '/dev/tty.usbmodemTDC1_00141', logging=False)
-
+    show_source_properties(
+        dev_path='/dev/tty.usbmodemTDC1_00141', logging=False)

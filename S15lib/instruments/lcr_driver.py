8348226da62fa2e33879749f5eca864015167b1e
Mathias Seidler <1469961+mathiasseidler@users.noreply.github.com>
2021-03-24 16:48:29 +0800

    Updated docstrings

--- S15lib/instruments/lcr_driver.py
+++ S15lib/instruments/lcr_driver.py
@@ -24,31 +24,60 @@ class LCRDriver(object):
         '''Resets the device.
 
         Returns:
-            str -- Response of the device after.
+            str: Response of the device after.
         '''
         return self._com.write(b'*RST')
 
     def all_channels_on(self):
+        """Switches all channels on, to a frequency of 2000 and switches off the LED.
+        """        
         self._com.write(b'ON\r\n')
         self._com.write(b'DARK\r\n')
         self._com.write(b'FREQ 2000\r\n')
 
     def set_voltage(self, channel: int, voltage: float):
+        """Sets the voltage of an output channel.
+
+        Args:
+            channel (int): channel number. Select from 1 to 4.
+            voltage (float): Set voltage in volts.
+
+        Raises:
+            Exception: When the voltage is below 0 volts or higher than 10 volts.
+        """
         if voltage < 10 and voltage >= 0:
             self._com.write(('AMPLITUDE {} {}\r\n'.format(channel, voltage)).encode())
         else:
             raise Exception('Voltage to high')
 
-    def read_voltage(self, channel: int):
+    def read_voltage(self, channel: int) -> float:
+        """Returns voltage of a channel
+
+        Args:
+            channel (int): Select from 1 to 4.
+
+        Returns:
+            float: Voltage of the selected channel.
+        """
         cmd = 'AMP? ' + str(channel)
-        return self._com._getresponse(cmd)
+        return float(self._com._getresponse(cmd))
 
     @property
     def V1(self):
-        return self._com._getresponse_1l('AMP? 1')
+        """Property which shows voltage of channel 1.
+
+        Returns:
+            float: voltage in volts
+        """        
+        return float(self._com._getresponse_1l('AMP? 1'))
 
     @V1.setter
-    def V1(self, V1):
+    def V1(self, V1: float):
+        """Property setter of the voltage of channel 1.
+
+        Args:
+            V1 (float): Voltage in volts.
+        """
         self._com.write(('AMPLITUDE 1 {}\r\n'.format(V1)).encode())
 
     @property
@@ -76,7 +105,12 @@ class LCRDriver(object):
         self._com.write(('AMPLITUDE 4 {}\r\n'.format(value)).encode())
 
     @property
-    def identity(self):
+    def identity(self) -> str:
+        """Returns identy string. Contains information about the device.
+
+        Returns:
+            str: Identity string.
+        """
         return self._com._getresponse_1l('*idn?')
 
     def help(self):

35c548b2753e590b5b26052b9918c64cac49208f
Mathias Seidler <1469961+mathiasseidler@users.noreply.github.com>
2021-04-22 16:19:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='S15lib',
-    version='0.1',
+    version='0.1.1',
     description='S-Fifteen Python Library',
     url='https://s-fifteen.com/',
     author='Mathias Seidler;',

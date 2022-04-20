7eca6bd85be5c1a3f732dde0def1c66093e9a464
syed82ab <syed.aljunid@gmail.com>
2022-04-20 10:05:52 +0800

    Added status property
    Added function to find out if laser is on or off from STATUS? command

--- S15lib/instruments/spdc_driver.py
+++ S15lib/instruments/spdc_driver.py
@@ -466,3 +466,15 @@ class SPDCDriver(object):
 
     def _power_off_laser(self) -> None:
         self.power &= 0b01
+
+    @property
+    def status(self) -> int:
+        """Returns the sst.loopstates
+            hloop 1 --> 1   (0b 0001)
+            ploop 1 --> 2   (0b 0010)
+            on      --> 4   (0b 0100)
+            Power 1 --> 256 (0b01 0000 0000)
+            Power 2 --> 512 (0b10 0000 0000)
+        """
+        return int(self._com.getresponse("STATUS?"))
+

3d1cb367dac7223a549fa570f7127b4553799427
Syed Abdullah Aljunid <syed.aljunid@gmail.com>
2022-12-08 22:43:25 +0800

    Added fixed DPG and fixed lint

--- S15lib/instruments/__init__.py
+++ S15lib/instruments/__init__.py
@@ -5,8 +5,8 @@ from .single_photon_detector import SinglePhotonDetector
 from .spdc_driver import SPDCDriver
 from .stepper_motor_driver import StepperMotorDriver
 from .timestamp7 import TimestampTDC2
-from .usb_counter_fpga import TimeStampTDC1
 from .universal_discriminator import UniversalDiscriminator
+from .usb_counter_fpga import TimeStampTDC1
 
 __all__ = [
     "LCRDriver",

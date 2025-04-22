bcf4278700c483713bcd8912b9419454b63af260
pyuxiang <pehyuxiang@gmail.com>
2025-04-22 13:49:12 +0800

    Reduce g2lib nesting
    e.g. 'S15lib.g2lib.g2lib' calls can now be replaced by 'S15lib.g2lib', to reduce unnecessary verbosity.

---
+++ S15lib/g2lib/__init__.py
@@ -0,0 +1,27 @@
+from S15lib.g2lib.g2lib import (
+    PeakStatistics,
+    cond_delta_loop,
+    cond_g2_extr,
+    delta_loop,
+    g2_extr,
+    generate_fft,
+    get_statistics,
+    get_xcorr,
+    histogram,
+    histogram_fft,
+    peak_finder,
+)
+
+__all__ = [
+    "PeakStatistics",
+    "cond_delta_loop",
+    "cond_g2_extr",
+    "delta_loop",
+    "g2_extr",
+    "generate_fft",
+    "get_statistics",
+    "get_xcorr",
+    "histogram",
+    "histogram_fft",
+    "peak_finder",
+]

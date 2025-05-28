447822f37f9d93fa1c227cfd65cfd83953e7e8b6
Justin <pehyuxiang@gmail.com>
2025-05-28 16:23:57 +0800

    Fix default compiler search to use preferred linker flags (#23)
    * fix: prefer use of default compile options to allow correct linking
    
    * Refactor compiler search into dedicated function
    
    * Update README to disable compilation during install

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import shlex
 import shutil
 import sysconfig
 
@@ -28,38 +29,102 @@ requirements_apps = [
 #   1. User environment variable, e.g. CC=gcc
 #   2. OS executable in PATH, e.g. /usr/bin/cc
 #   3. Python build-time compiler
-config = sysconfig.get_config_vars()
-cc = os.environ.get("CC", shutil.which("cc"))
-if cc is not None:
-    config["CC"] = cc
-    config["LDSHARED"] = f"{cc} -shared"  # for shared libraries
-ldshared = os.environ.get("LDSHARED", None)  # allow user override
-if ldshared is not None:
-    config["LDSHARED"] = ldshared
+#
+# TODO: Check if compatible with Windows installations
 
+
+def get_executable_index(tokens, check_exists=True):
+    for i, token in enumerate(tokens):
+        if "=" not in token:  # ignore environment variables
+            if check_exists and not shutil.which(token):
+                continue
+            return i
+    return None
+
+
+def replace_executable(target, command):
+    tokens = shlex.split(command)
+    idx = get_executable_index(tokens, check_exists=False)
+    tokens = list(tokens)  # replace with mutable container
+    tokens[idx] = target
+    return shlex.join(tokens)
+
+
+def find_executable(command):
+    tokens = shlex.split(command)
+    idx = get_executable_index(tokens)
+    return idx is not None
+
+
+def get_compiler(config=sysconfig.get_config_vars(), env={}):
+    """Returns preferred cc and ldshared programs.
+
+    Args:
+        config: config dictionary from Python's sysconfig.
+        env: environment variables.
+
+    Examples:
+        $ export PROG="python -c 'import setup, os; \
+            print(setup.get_compiler(setup.config, os.environ))'"
+
+        # Assuming gcc build compiler, not present on host
+        $ eval $PROG
+        ('cc -pthread', 'cc -pthread -shared -Wl,--exclude-libs,ALL -LModules/_hacl')
+
+        # Creating a symlink named 'gcc'
+        $ ln -s /usr/bin/cc /usr/bin/gcc
+        $ eval $PROG
+        ('gcc -pthread', 'gcc -pthread -shared -Wl,--exclude-libs,ALL -LModules/_hacl')
+
+        # Use env-defined compilers
+        $ CC='clang -Wall' eval $PROG
+        ('clang -Wall', 'clang -Wall -shared')
+        $ CC=clang LDSHARED='clang -shared -O2' eval $PROG
+        ('clang', 'clang -shared -O2')
+    """
+
+    # Prefer user-supplied compiler (flags)
+    if "CC" in env:
+        cc = env.get("CC")
+        return cc, env.get("LDSHARED", f"{cc} -shared")
+
+    platform_cc = shutil.which("cc")
+    cc = config.get("CC", platform_cc)
+    ldshared = config.get("LDSHARED", f"{platform_cc} -shared")
+    if not find_executable(cc):  # replace build compiler with platform's
+        return (
+            replace_executable(platform_cc, cc),
+            replace_executable(platform_cc, ldshared),
+        )
+    return cc, ldshared  # use default compiler
+
+
+config = sysconfig.get_config_vars()
+config["CC"], config["LDSHARED"] = get_compiler(config, os.environ)
 
 # setuptools project configuration
-setuptools.setup(
-    name="S15lib",
-    version="0.2.0",
-    description="S-Fifteen Python Library",
-    url="https://s-fifteen.com/",
-    author="Mathias Seidler;",
-    author_email="",
-    license="MIT",
-    packages=setuptools.find_packages(),
-    install_requires=requirements,
-    extras_require={
-        "dev": requirements_dev,
-        "apps": requirements_apps,
-    },
-    python_requires=">=3.6",
-    ext_modules=[
-        setuptools.Extension(
-            name="S15lib.g2lib.delta",
-            sources=["S15lib/g2lib/delta.c"],
-            include_dirs=[np.get_include()],
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-        )
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup(
+        name="S15lib",
+        version="0.2.0",
+        description="S-Fifteen Python Library",
+        url="https://s-fifteen.com/",
+        author="Mathias Seidler;",
+        author_email="",
+        license="MIT",
+        packages=setuptools.find_packages(),
+        install_requires=requirements,
+        extras_require={
+            "dev": requirements_dev,
+            "apps": requirements_apps,
+        },
+        python_requires=">=3.6",
+        ext_modules=[
+            setuptools.Extension(
+                name="S15lib.g2lib.delta",
+                sources=["S15lib/g2lib/delta.c"],
+                include_dirs=[np.get_include()],
+                define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            )
+        ],
+    )
